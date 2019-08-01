34196b2687f0b0a1452f22804af4f2e3d28a3859
Hamza Qureshi <hamza.qureshi97@gmail.com>
2019-08-01 17:55:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-function
-function keepGoing (array, changeValue, skipValue){
-<<<<<<< HEAD
-  for( var i=0; i<array.length; i++){
-=======
-  for( i=0; i<array.length; i++){
->>>>>>> 7477b1f3cbc4b415089d54103e395db897207925
-    if(array[i] === skipValue){
-      continue
+function breakOut ( array, changeValue, stopValue){
+  for ( i = 0; i < array.length; i++){
+    if ( array[i] === stopValue){
+        break
     }
     array[i] = changeValue
   }
-  return array
-}
-
-<<<<<<< HEAD
-function findBy(array, findFn) {
-  for (let i = 0, l = array.length; i < l; i++) {
-    if (findFn(array[i])) {
-      return array[i]
-    }
-  }
-  return null
-=======
-function findBy (array, findFn) {
-  for( i=0; i<array.length; i++){
-    if(findFn(array[i])){
-      return array
-    }
-}
-return null
->>>>>>> 7477b1f3cbc4b415089d54103e395db897207925
 }
